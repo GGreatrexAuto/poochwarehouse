d0be86bbd1a5dedab4c55dd43c7f1ddcc4721b90
GarethGreatrex <grgreatrex@hotmail.com>
2019-07-06 16:54:19 +0100

    added header menu pageelement class and initial  webelement for header bar.

--- PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/HomePage.cs
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/HomePage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenQA.Selenium;
 
 namespace PoochWareHouse_Automation.Pages
 {
@@ -11,5 +12,7 @@ namespace PoochWareHouse_Automation.Pages
         {
             
         }
+
+        //IWebElement PoochWareHouseLogo => Driver._webdriver.FindElement(By.Id())
     }
 }
---
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/PageElements/HeaderMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace PoochWareHouse_Automation.Pages.PageElements
+{
+    public class HeaderMenu
+    {
+        private IWebElement PoochWareHouseLogo => Driver._webdriver.FindElement(By.Id("fsb.bar"));
+    }
+}
