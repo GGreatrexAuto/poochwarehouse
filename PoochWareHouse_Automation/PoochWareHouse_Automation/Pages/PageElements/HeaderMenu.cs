bde75d32cd2769ac2968aee65d73ba0c83cbeecf
GarethGreatrex <grgreatrex@hotmail.com>
2019-07-17 12:40:33 +0100

    Added elements for footer and header. Also moved around a few classes

--- PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/PageElements/HeaderMenu.cs
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/PageElements/HeaderMenu.cs
@@ -8,6 +8,14 @@ namespace PoochWareHouse_Automation.Pages.PageElements
 {
     public class HeaderMenu
     {
-        private IWebElement PoochWareHouseLogo => Driver._webdriver.FindElement(By.Id("fsb.bar"));
+        public IWebElement PoochWareHouseLogo => Driver._webdriver.FindElement(By.Id("fsb.bar"));
+        public IWebElement HomeLink => Driver._webdriver.FindElement(By.XPath("//*[@id=\"SiteNav\"]/li[1]/a/span"));
+        public IWebElement OnSaleLink => Driver._webdriver.FindElement(By.XPath("//*[@id=\"SiteNav\"]/li[2]/a/span"));
+        public IWebElement CollectionsDropDownLink => Driver._webdriver.FindElement(By.XPath("//*[@id=\"SiteNav\"]/li[3]/button/span"));
+        public IWebElement AllProductsLink => Driver._webdriver.FindElement(By.XPath("//*[@id=\"SiteNav\"]/li[4]/a/span"));
+        public IWebElement Search => Driver._webdriver.FindElement(By.XPath("//*[@id=\"shopify-section-header\"]/div[2]/header/div/div[2]/div/button[1]/svg"));
+        public IWebElement Login => Driver._webdriver.FindElement(By.XPath("//*[@id=\"shopify-section-header\"]/div[2]/header/div/div[2]/div/a[1]/svg"));
+        public IWebElement Basket => Driver._webdriver.FindElement(By.XPath("//*[@id=\"shopify-section-header\"]/div[2]/header/div/div[2]/div/a[2]/svg"));
+        
     }
 }
---
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/PageElements/QuickLinksFooter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace PoochWareHouse_Automation.Pages.PageElements
+{
+    public class QuickLinksFooter
+    {
+        public IWebElement QuickLinksHeading =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/p"));
+
+        public IWebElement NewsletterHeading =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[2]/div/p"));
+
+        public IWebElement AboutUsLink =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/ul/li[1]/a"));
+
+        public IWebElement FAQLink =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/ul/li[2]/a"));
+
+        public IWebElement DeliveryAndShippingLink =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/ul/li[3]/a"));
+
+        public IWebElement ReturnsPolicyLink =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/ul/li[4]/a"));
+
+        public IWebElement PrivacyPolicyLink =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/ul/li[5]/a"));
+
+        public IWebElement TermsOfUseLink =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/ul/li[6]/a"));
+
+        public IWebElement ContactUsLink =>
+            Driver._webdriver.FindElement(
+                By.XPath("//*[@id=\"shopify-section-footer\"]/footer/div[1]/div/div[1]/div/ul/li[7]/a"));
+
+        public IWebElement NewsLetterEmailAddressField => Driver._webdriver.FindElement(By.Id("ContactFooter-email"));
+
+        public IWebElement SubscribeButton => Driver._webdriver.FindElement(By.Id("Subscribe"));
+
+    }
+}
