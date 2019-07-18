594866f15712fe8dc8c2374270a68e2bad88fa66
GarethGreatrex <grgreatrex@hotmail.com>
2019-07-18 07:45:32 +0100

    Added classes for each informational page and added page element for page heading to each class.
    Added navigational class for footer links.

---
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Navigation/FooterNavigation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoochWareHouse_Automation.Pages.PageElements;
+
+namespace PoochWareHouse_Automation.Navigation
+{
+    public class FooterNavigation
+    {
+        private QuickLinksFooter _footerLinks;
+
+        public FooterNavigation()
+        {
+            _footerLinks = new QuickLinksFooter();
+        }
+
+        public void ToFaq()
+        {
+            _footerLinks.FAQLink.Click();   
+        }
+
+        public void ToAboutUs()
+        {
+            _footerLinks.AboutUsLink.Click();
+        }
+
+        public void ToDeliveryAndShipping()
+        {
+            _footerLinks.DeliveryAndShippingLink.Click();
+        }
+
+        public void ToReturnsPolicy()
+        {
+            _footerLinks.ReturnsPolicyLink.Click();
+        }
+
+        public void ToPrivacyPolicy()
+        {
+            _footerLinks.PrivacyPolicyLink.Click();
+        }
+
+        public void ToTermsOfUse()
+        {
+            _footerLinks.TermsOfUseLink.Click();
+        }
+
+        public void ToContactUs()
+        {
+            _footerLinks.ContactUsLink.Click();
+        }
+    }
+}
---
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/InformationalPages/Faq.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace PoochWareHouse_Automation.Pages.InformationalPages
+{
+    public class Faq
+    {
+        public IWebElement FaqPageHeading =>
+            Driver._webdriver.FindElement(By.XPath("//*[@id=\"MainContent\"]/div/div/div/div[1]/h1"));
+    }
+}
