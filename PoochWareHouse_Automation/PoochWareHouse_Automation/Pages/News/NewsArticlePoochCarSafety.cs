515f6978894e5d7315a7e50feff4d76ceaf20d22
GarethGreatrex <grgreatrex@hotmail.com>
2019-07-30 18:35:12 +0100

    added another news article class.

---
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/News/NewsArticlePoochCarSafety.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoochWareHouse_Automation.Pages.News
+{
+    public static class NewsArticlePoochCarSafety
+    {
+
+    }
+}
