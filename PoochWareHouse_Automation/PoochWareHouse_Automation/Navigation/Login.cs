7d391b36e0995ee117e89365a897300f1177677c
GarethGreatrex <grgreatrex@hotmail.com>
2019-08-10 09:06:58 +0100

    added login form elements and navigational methods.

---
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Navigation/Login.cs
@@ -0,0 +1,22 @@
+﻿using PoochWareHouse_Automation.Pages;
+
+namespace PoochWareHouse_Automation.Navigation
+{
+    public static class Login
+    {
+        public static void ClickCreateAccount()
+        {
+            LoginForm.CreateAccountLink.Click();
+        }
+
+        public static void ClickForgotPassword()
+        {
+            LoginForm.ForgotPasswordLink.Click();
+        }
+
+        public static void ClickLoginButton()
+        {
+            LoginForm.LoginButton.Click();
+        }
+    }
+}
--- PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/LoginForm.cs
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Pages/LoginForm.cs
@@ -5,5 +5,15 @@ namespace PoochWareHouse_Automation.Pages
     public static class LoginForm
     {
         public static IWebElement LoginPageheader => Driver._webdriver.FindElement(By.Id("LoginHeading"));
+
+        public static IWebElement ForgotPasswordLink => Driver._webdriver.FindElement(By.Id("RecoverPassword"));
+
+        public static IWebElement CreateAccountLink => Driver._webdriver.FindElement(By.Id("customer_register_link"));
+
+        public static IWebElement LoginButton => Driver._webdriver.FindElement(By.XPath("//*[@id=\"customer_login\"]/div/input"));
+
+        public static IWebElement UserNameField => Driver._webdriver.FindElement(By.Id("CustomerEmail"));
+
+        public static IWebElement PasswordField => Driver._webdriver.FindElement(By.Id("CustomerPassword"));
     }
 }
