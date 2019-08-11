174e28bf7e3f6bb78dc5b41bcbb6958246b889b8
Gareth <grgreatrex@hotmail.com>
2019-08-11 07:52:43 +0100

    added complete login form username and password methods

--- PoochWareHouse_Automation/PoochWareHouse_Automation/Navigation/Login.cs
+++ PoochWareHouse_Automation/PoochWareHouse_Automation/Navigation/Login.cs
@@ -18,5 +18,15 @@ namespace PoochWareHouse_Automation.Navigation
         {
             LoginForm.LoginButton.Click();
         }
+
+        public static void CompleteUserNameField(string userName)
+        {
+            LoginForm.UserNameField.SendKeys(userName);
+        }
+
+        public static void CompletePasswordField(string password)
+        {
+            LoginForm.PasswordField.SendKeys(password);
+        }
     }
 }
